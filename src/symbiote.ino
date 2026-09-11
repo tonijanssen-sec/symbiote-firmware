332d0819516e36a8d1dfa97a3a23c6b71d0ee32f
Toni Janßen <tonijanssen1992@hotmail.com>
2026-09-11 23:28:06 +0200

    add working OLED display code

--- src/symbiote.ino
+++ src/symbiote.ino
@@ -1,8 +1,23 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire, -1);
+
 void setup() {
-  Serial.begin(115200);
+  Wire.setSDA(4);
+  Wire.setSCL(5);
+  Wire.begin();
+  delay(250);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.display();
+  delay(100);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Hello Symbiote");
+  display.display();
 }
 
 void loop() {
-  Serial.println("Symbiote alive");
-  delay(1000);
 }
