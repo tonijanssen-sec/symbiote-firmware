e89f0f1a5b4780b1e60d2663f19d9d79f8132b05
Toni Janßen <tonijanssen1992@hotmail.com>
2026-09-12 15:52:08 +0200

    add button driver menu navigation

--- src/symbiote.ino
+++ src/symbiote.ino
@@ -4,6 +4,14 @@
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire, -1); //Display object 128x64, I2C, no reset-pin
 
 const int buttonUp = 22; // GPIO22 = the up botton (HackBat 1.0 schematics)
+const int buttonLeft = 26;
+const int buttonEnter = 27;
+const int buttonRight = 28;
+const int buttonDown = 29;
+
+const char* menuItems[]{"Scanner", "Settings", "About"}; // menu list
+const int menuLength = 3; 
+int selectedIndex = 0; // which item is currently selected 
 
 void setup() {
   // setting I2C Pins (SDA/SCL see HackBat 1.0 schematics)
@@ -13,6 +21,10 @@ void setup() {
   delay(250);
 
   pinMode(buttonUp, INPUT); // Button pin as entrance configuration
+  pinMode(buttonLeft, INPUT);
+  pinMode(buttonEnter, INPUT);
+  pinMode(buttonRight, INPUT);
+  pinMode(buttonDown, INPUT);
 
   // initialize display 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -25,17 +37,33 @@ void setup() {
 }
 
 void loop() {
-  int state = digitalRead(buttonUp); // read button state: HIGH = pressed, LOW released
-  display.clearDisplay();
-  display.setCursor(0, 0);
+  int stateUp = digitalRead(buttonUp); // read button state: HIGH = pressed, LOW released
+  int stateLeft = digitalRead(buttonLeft);
+  int stateEnter = digitalRead(buttonEnter);
+  int stateRight = digitalRead(buttonRight);
+  int stateDown = digitalRead(buttonDown);
 
-  // shows you text depending on button state
-  if (state==HIGH) {
-    display.println("Up pressed!");
-  } else {
-    display.println("Waiting...");
+  if (stateUp == HIGH) {
+    selectedIndex--; // move cursor up
+    if (selectedIndex < 0) selectedIndex = menuLength - 1; // wrap to bottom (basically goes from index 0 to last item, index 2 = About)
+    delay(200); // simple delay so it doesn't scroll too fast
   }
 
+  if (stateDown == HIGH) {
+    selectedIndex++; // move cursor down
+    if (selectedIndex >= menuLength) selectedIndex = 0; // wrap to top (same logic as stateUp, just reversed)
+    delay(200);
+  }
+
+  display.clearDisplay();
+  for (int i = 0; i < menuLength; i++) {
+    display.setCursor(0, i * 10); // column distance so the items don't overlap in the menu, 0=0, 1=10, 2=20
+    if (i == selectedIndex) {
+      display.print("> "); // draw arrow if this line is the selected one
+    } else {
+      display.print("  "); // otherwise draw blank space so text keeps aligned
+    }
+    display.println(menuItems[i]);
+  }
   display.display();
-  delay(100);
 }
